6df9e3509c5656cbce0d1624a6e3cdaf18e45f4c
Unknown <wuerbo@gmail.com>
2020-02-01 13:24:43 +0100

    Initial commit

---
+++ Assets/Scripts/station.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class station : MonoBehaviour
+{
+    public bool blocked = false;
+    public GameObject weapon;
+
+    void inputWeapon(GameObject weapon) {
+        if (blocked == false) {
+            blocked = true;
+            // play animation
+            this.weapon = weapon;
+            // gameObject.GetComponent<Animation>().Play();
+            // release
+            blocked = false;
+        }
+    }
+
+    void retrieveWeapon() {
+        if (blocked == false) {
+            
+        }
+    }
+
+}
