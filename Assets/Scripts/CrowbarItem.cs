e529d07de801077f3a551192a47d26b0c1f5d64c
PabloM <wuerbo@gmail.com>
2020-02-01 23:23:59 +0100

    added stations

---
+++ Assets/Scripts/CrowbarItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowbarItem : Item
+{
+    public CrowbarItem()
+    {
+        this.stationsLeft.Enqueue("Forge");
+        this.stationsLeft.Enqueue("Anvil");
+        this.stationsLeft.Enqueue("Forge");
+        this.stationsLeft.Enqueue("Barrel");
+        //this.currentStation = "Forge";
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/ItemHolder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHolder : MonoBehaviour
+{
+    public Vector3 spawnPosition;
+    public List<GameObject> items;
+    public int maxItems = 2;
+
+    public ItemHolder() {
+        
+    }
+
+    public void insert(GameObject item) {
+        print("Item received at the " + gameObject.name);
+        items.Add(item);
+        item.GetComponent<Item>().setLocation(gameObject);
+
+        // move position
+        Vector3 newItemPosition = transform.position + new Vector3(0, 0, -2);
+        item.transform.position = newItemPosition;
+    }
+
+    public void remove(GameObject item)
+    {
+        this.items.Remove(item);
+    }
+
+    public bool isFull()
+    {
+        return items.Count == maxItems;
+    }
+
+}
