de2c57c5a62e6ca58f913fc584b12b937dfc39f5
PabloM <wuerbo@gmail.com>
2020-02-05 00:46:14 +0100

    added timer animation, cleaned prefabs

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,15 +3,34 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour {
+    private float originalTimer;
+    private int currentProgress = 0;
+
     public float timer = 2.00f;
     public GameObject scoreManager;
+    public Sprite[] timerSprites;
+
+    void Start() {
+        originalTimer = timer;
+    }
+
     void Update()
     {
         timer -= Time.deltaTime;
-        if ( timer <= 0)
+
+        int progress = Mathf.FloorToInt((1 - timer / originalTimer) / 0.125f);
+
+        if (timer <= 0)
         {
             handleGameOver();
         }
+
+        if (progress > currentProgress) {
+            print("[-] " + progress);
+            updateTimerSprites(progress);
+        }
+
+
     }
 
     void handleGameOver() {
@@ -28,4 +47,11 @@ public class Timer : MonoBehaviour {
         
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
+
+    private void updateTimerSprites(int progress)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = timerSprites[progress];
+        currentProgress = progress;
+    }
+
 } 
